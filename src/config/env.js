f6a248bb2485a7fe15c94b9f85db7837fad33ae3
William Daniel Dias <daniel.willia@hotmail.com>
2025-10-22 18:23:38 -0300

    implementada interracao com o env

---
+++ src/config/env.js
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export const env = {
+    port: Number (process.env.PORT ?? 333),
+    jwtSecret: process.env.JWT_SECRET ?? "change-me",
+    jwrExpiresIn: process.env.JWT_EXPIRES_IN ?? "15m",
+    corsOrigin: process.env.CORS_ORIGIN ?? "*",
+}
