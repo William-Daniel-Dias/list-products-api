3fbe05e3a3feb57e724993865e1a55ae75fc5fb6
William Daniel Dias <daniel.willia@hotmail.com>
2025-10-27 19:04:15 -0300

    finalizado controler

---
+++ src/modules/products/product.controller.js
@@ -0,0 +1,63 @@
+import { makeProductService } from "./product.service.js";
+
+export const makeProductController = () => {
+    const service = makeProductService();
+
+    const create = async (request, response, next) => {
+        try {
+            const { name, price } = request.body;
+
+            const product = await service.create({
+                name,
+                price,
+                userId: request.user.id
+            });
+
+            return response.status(201).json(product);
+
+        } catch (error) {
+            return next(error);
+        }
+    };
+
+    const list = async (request, response, next) => {
+        try {
+            const data = await service.list(request.query);
+
+            return response.json(data);
+
+        } catch (error) {
+            return next(error);
+        }
+    };
+
+    const get = async (request, response, next) => {
+        try {
+            return response.json(await service.get(Number(request.params.id)));
+
+        } catch (error) {
+            return next(error);
+
+        }
+    };
+
+    const patch = async (request, response, next) => {
+        try {
+            return response.json(await service.patch(Number(request.params.id), request.body));
+        }
+        catch (error) {
+            return next(error);
+        }
+    };
+
+    const remove = async (request, response, next) => {
+        try {
+            await service.remove(Number(request.params.id)); return response.status(204).send();
+        }
+        catch (error) {
+            return next(error);
+        }
+    };
+
+    return { create, list, get, patch, remove };
+};
